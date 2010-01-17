c0fb68de97a82f3d5f36aad5ba330dd114868d51
Alex Bennee <alex@bennee.com>
2010-01-17 15:07:02 +0100

    Add basic PHP mode hooks

--- my-devel.el
+++ my-devel.el
@@ -92,3 +92,39 @@
 (setq auto-mode-alist
       (append (list (cons "Makefile\.*" 'makefile-gmake-mode))
 	      auto-mode-alist))
+
+;; PHP
+;
+
+(defun my-wordpress-hook ()
+  "Hook function for editing Wordpress code"
+  (interactive)
+  (setq tab-width 4
+	c-basic-offset 4
+	indent-tabs-mode 't))
+
+(defvar my-php-hooks-alist
+  (mapc
+   (lambda (elt)
+     (cons (purecopy (car elt)) (cdr elt)))
+   '(
+     (".*/wp-content.*php$" . my-wordpress-hook )
+     (".*/wordpress.*php$" . my-wordpress-hook )))
+  "A list of reg-ex to php-mode hooks")
+
+; You can add to the alist with something like:
+; (setq my-php-hooks-alist (cons '(".*/wp-content.*php$" . 'my-wordpress-hook ) my-php-hooks-alist))
+; (assoc-default "/path/to/wp-content/test.php" my-php-hooks-alist 'string-match) => my-wordpress-hook
+
+(defun my-php-mode-hook()
+  "Run hooks for PHP mode"
+  (interactive)
+  (if (buffer-file-name)
+      (let ((f (assoc-default 
+		(buffer-file-name) my-php-hooks-alist
+		'string-match)))
+	(message (format "my-php-mode-hook: got %s" f))
+	(if (functionp f)
+	    (funcall f)))))
+
+(add-hook 'php-mode-hook 'my-php-mode-hook)
