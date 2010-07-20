1811cc78604d4abb2518b63c5c0245ce1b8f485a
Alex Bennee <alex@bennee.com>
2010-07-20 20:23:53 +0100

    Make the property list values straight lists without clever quoteing

--- my-eproject.el
+++ my-eproject.el
@@ -10,10 +10,10 @@
   (and
    (look-for "Makefile.cleanenv")
    (look-for "build-system"))
-  :common-compiles ('("make build-nms PLATFORM=Linux_Desktop"
-		      "make build-nms PLATFORM=Linux_Desktop EMSDEBUG=1"
-		      "make pkg-nms PLATFORM=Linux_Desktop"
-		      "make release PLATFORM=Linux_OE_RC")))
+  :common-compiles ("make build-nms PLATFORM=Linux_Desktop"
+		    "make build-nms PLATFORM=Linux_Desktop EMSDEBUG=1"
+		    "make pkg-nms PLATFORM=Linux_Desktop"
+		    "make release PLATFORM=Linux_OE_RC"))
 
 					;      (add-hook cbnl-tree-project-file-visit-hook '(lambda ()
 					;						    (require 'cbnl)))
@@ -21,15 +21,18 @@
 (define-project-type debian-package
   (generic)
   (look-for "debian")
-  :common-compiles ('("dpkg-buildpackage -rfakeroot")))
+  :common-compiles ("dpkg-buildpackage -rfakeroot"))
 
 (define-project-type android-package
   (generic)
   (look-for "AndroidManifest.xml")
-  :common-compiles ('("ant compile")))
+  :common-compiles ("ant compile"))
 
 (define-project-type rockbox
   (generic-git)
   (look-for "../rockbox.git/rbutil")
-  :common-compiles ('("make" "make install" "make fullzip")))
+  :common-compiles ("make" "make install" "make fullzip"))
 
+(global-set-key (kbd "C-c c") 'eproject-compile)
+(global-set-key (kbd "C-c r") 'recompile)
+  
