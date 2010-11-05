3f97c99f1a1afe12463e17d7d2055ace9d29a3ec
Alex Bennee <alex@bennee.com>
2010-11-05 11:10:37 +0000

    Just (require 'cedet) if the version of emacs is new enough

--- my-onms.el
+++ my-onms.el
@@ -5,19 +5,22 @@
 
 (message "Loading OpenNMS customisations")
 
-;; JDEE requires CEDET
+;; JDEE requires CEDET, however bleeding edge emacs packages
+; its own version.
+(if (> emacs-major-version 23)
+    (require 'cedet)
+  (unless (featurep 'cedet) 
+	(if (file-exists-p "/export/src/cedet.cvs/common/cedet.el")
+	    (load-file "/export/src/cedet.cvs/common/cedet.el"))))
 
-(if (file-exists-p "/export/src/cedet.cvs/common/cedet.el")
-    (load-file "/export/src/cedet.cvs/common/cedet.el"))
-
-(add-to-list 'load-path (expand-file-name
-			 "/export/src/jdee/jdee-2.4.0.1/lisp"))
-
-(require 'jde)
-
-(jde-set-variables
- '(jde-jdk-registry '(("1.6.1" . "/usr/lib/jvm/java-6-openjdk/")))
- '(jde-jdk (quote ("1.6.1"))))
+;;(add-to-list 'load-path (expand-file-name
+;;			 "/export/src/jdee/jdee-2.4.0.1/lisp"))
+;;
+;;(require 'jde)
+;;
+;; (jde-set-variables
+;;  '(jde-jdk-registry '(("1.6.1" . "/usr/lib/jvm/java-6-openjdk/")))
+;;  '(jde-jdk (quote ("1.6.1"))))
 
 ; Java stuff
 ; (OpenNMS standards http://www.opennms.org/index.php/Code_conventions)
@@ -38,6 +41,7 @@
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "<f3>")  'compile)
 
+; only for old style...
 (if (boundp 'current-project-root)
     (setq compile-command (format
 			   "cd %s && ./build.sh install assembly:directory-inline"
