d8574b90741c408db7169d75a92add95f3e622c6
Alex Bennee <alex@bennee.com>
2010-11-05 11:08:38 +0000

    Move .emacs to init.el so everything can be kept in one dir

--- setup_emacs.sh
+++ setup_emacs.sh
@@ -9,18 +9,6 @@
 # This script assumes its in the repo and all dotfiles need to go to $HOME
 #
 
-for file in dot*
-do
-  dotfile=`echo $file | sed s/dot/\./`
-  if [ -f $HOME/$dotfile ] ; then
-    echo "backing up $HOME/$dotfile"
-    mv $HOME/$dotfile $HOME/$dotfile.orig
-  fi
-  linkfile=`pwd`/$file
-  echo "Linking $linkfile to ~/$dotfile"
-  ln -s $linkfile ~/$dotfile
-done
-
 #
 # Create the ./emacs.d
 #
