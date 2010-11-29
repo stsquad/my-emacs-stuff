0466ca17373750e09c256dcdb178ea3f8e813a29
Alex Bennee <alex@bennee.com>
2010-11-29 11:31:42 +0000

    Add my-eproject-find and define a kernel project

--- my-eproject.el
+++ my-eproject.el
@@ -50,8 +50,17 @@
   (look-for "manifest.json"))
 
 (add-hook 'chrome-extension-visit-hook '(lambda ()
-					  (maybe-load-library "js2-mode")))
+					  (maybe-load-library "js2-mode")
+					  (setq tab-width 2
+						c-basic-offset 2
+						indent-tabs-mode 't)))
 
+(define-project-type kernel
+  (generic-git)
+  (look-for "Documentation/CodingStyle")
+  :common-compile ("ARCH=x86 make" "make" "ARCH=x86 make TAGS"))
+
+(add-hook 'kernel-visit-hook '(lambda () require 'my-c-mode))
 
 ; Hook in compile
 (global-set-key (kbd "C-c c") 'compile)
@@ -65,4 +74,24 @@
 (add-hook 'c-mode-common-hook 'eproject-maybe-turn-on)
 (add-hook 'makefile-mode-hook 'eproject-maybe-turn-on)
 (add-hook 'java-mode-hook 'eproject-maybe-turn-on)
+
+(defun my-eproject-find (&optional search)
+  "Do a find across the project"
+  (interactive "sSearch string:")
+
+  (if (file-exists-p (concat eproject-root "/.git"))
+      (let ((buffer (concat "*git grep for " search "*" ))
+	    (command (concat "git grep -n " search)))
+	(setq grep-command "git grep -n ")
+	(message "Using git grep for searches")
+	(shell-command command buffer)
+	(pop-to-buffer buffer)
+	(grep-mode)))
+  (grep-find (concat
+	      "find "
+	      eproject-root " -print0 |  xargs -0 -e grep -n -e " search)))
+  
+
+(unless (global-key-binding (kbd "<f5>"))
+  (global-set-key (kbd "<f5>") 'my-eproject-find))
 
