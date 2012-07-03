1f8d6f50960c40481b840841c5a56680c5b11ba2
Alex Bennee <ajb@cbnl.com>
2012-07-03 15:04:28 +0100

    init.el: use switch-to-buffer to keep status in main
    The default behaviour is pop-to-buffer which using another window
    which for me is not what I want.

--- init.el
+++ init.el
@@ -1034,7 +1034,8 @@ on the command line"
     (add-to-list 'vc-handled-backends 'Git)
   (setq vc-handled-backends (remq 'Git vc-handled-backends))
   (autoload 'magit-status "magit" "magit front end" t)
-  (global-set-key (kbd "C-x g") 'magit-status))
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (setq magit-status-buffer-switch-function 'switch-to-buffer))
 
 ; Also the git-blame and git-status stuff
 (if (locate-library "git")
