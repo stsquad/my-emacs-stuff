ea54332d614538eabab4d6853b034a9d6d099c1f
Alex Bennée <ajb@cbnl.com>
2012-07-26 14:41:05 +0100

    init.el: add MELPA to the list of archives

--- init.el
+++ init.el
@@ -175,6 +175,8 @@ on the command line"
   (package-initialize)
   (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
   ; list of packages I care about
   (defvar ajb-packages
