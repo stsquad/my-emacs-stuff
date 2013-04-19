c3cdfd01231ec3d2474b89441464b1e0a21467af
Alex Bennée <alex@bennee.com>
2013-04-19 02:04:56 +0100

    init.el: more aggressive tramp customisation for high latency networks
    Running over a high latency network is a little dicey with tramp so I've set it up to assume the remote end has nothing going on to make the best use of caching within tramp.

--- init.el
+++ init.el
@@ -146,9 +146,25 @@ on the command line"
 (define-key global-map [menu-bar tools print ps-print-buffer]
   '("Postscript Print Buffer" . ps-print-buffer))
 
-;; You can pretty much guarantee tramp implies over ssh
-;(setq tramp-default-method "ssh")
-(setq tramp-default-method "scpc")
+;;
+;; TRAMP customisations.
+;;
+
+; You can pretty much guarantee tramp implies over ssh
+(setq tramp-default-method "scp")
+; Auto-saves tramp files on our local file system
+(add-to-list 'backup-directory-alist
+	     (cons tramp-file-name-regexp "~/.emacs.d/tramp-saves"))
+; If I'm travelling assume no one is messing with files on my work
+; machine
+(when (string-match "symbiot" (system-name))
+  (setq remote-file-name-inhibit-cache 'nil
+	tramp-completion-reread-directory-timeout 'nil))
+; I tend to use magit anyway, but disable VC mode over TRAMP
+(setq vc-ignore-dir-regexp
+      (format "\\(%s\\)\\|\\(%s\\)"
+	      vc-ignore-dir-regexp
+	      tramp-file-name-regexp))
 
 ;; Move the custom file out of init.el
 (setq custom-file "~/.emacs.d/my-custom.el")
