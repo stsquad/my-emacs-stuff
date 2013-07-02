5b9960f4b4678ec7866fcb8ad39d0057bba6b116
Alex Bennée <alex@bennee.com>
2013-07-02 12:39:49 +0100

    my-python-mode: move properly across to elpy

--- my-python-mode.el
+++ my-python-mode.el
@@ -1,15 +1,17 @@
 ;;
 ;; My python mode mods
 ;;
-
-; There are two python modes - this refers to the python supplied one
-;(require 'python-mode) ; hmm, broken
-
-;(when (maybe-load-library "ipython")
-;    (setq py-python-command-args '( "-colors" "Linux")))
+; There are two python modes, one supplied with Emacs (python.el) and
+; a Python community one called python-mode.el. elpy works with the
+; Emacs version. Annoyingly both these modes clash so hi-jinks can
+; ensue when your not sure what you are running.
 
 (when (require 'elpy 'nil 't)
+  (setq elpy-rpc-backend "jedi"
+	elpy-rpc-project-specific 't)
   (elpy-enable)
   (elpy-clean-modeline))
+
+;  (define-key elpy-mode-map (kbd "C-c C-f") 'elpy-eldoc-documentation))
 
 (message "Done loading my-python-mode")
