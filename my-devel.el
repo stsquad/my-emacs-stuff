c66286efbd2b1bd43239a9585e81d51c3a6d0b5c
Alex Bennée <alex@bennee.com>
2013-07-02 12:44:35 +0100

    my-devel: add generic project/sub-proj search
    The idea is to build on a basic level of project stuff defined in here.

--- my-devel.el
+++ my-devel.el
@@ -81,3 +81,62 @@
 	    (funcall f)))))
 
 (add-hook 'php-mode-hook 'my-php-mode-hook)
+
+;;
+;; Find methods
+;
+; I want two find methods:
+;   f5 - from the "project" root
+;   f6 - from where I am (or somewhere else)
+;
+
+(defvar my-find-history
+  '()
+  "History of searches I've done")
+
+(defun my-find-search-string()
+  "Return a historical based search string via ido"
+  (ido-completing-read "Search string:"
+		       my-find-history ; sources
+		       'nil ; predicate
+		       'nil ; require-match
+		       (thing-at-point 'symbol) ; initial input
+		       'my-find-history)) ;history
+
+(defun my-find-paramters()
+  "Return a root directory and search string combo"
+  (list
+   (read-directory-name "Root dir:")
+   (my-find-search-string)))
+  
+(defun my-grep-find (&optional root search)
+  (interactive (my-find-paramters))
+  (grep-find (concat
+	      "find "
+	      root " -print0 |  xargs -0 -e grep -n -e " search)))
+
+(defvar my-project-find-fallback-func
+  'my-grep-find
+  "The function I call to do the actual search")
+
+(defun my-project-find (&optional search)
+  "Search within the project"
+  (interactive (list (my-find-search-string)))
+  (funcall my-project-find-fallback-func default-directory search))
+
+(global-set-key (kbd "<f5>") 'my-project-find)
+(global-set-key (kbd "<f6>") '(lambda (root search)
+				(interactive (my-find-paramters))
+				(funcall my-project-find-fallback-func
+					 root search)))
+
+;
+; Now we have some basic defaults we might want to enhance if we have
+; certain packages available to us.
+;
+
+(when (require 'ack-and-a-half nil 'noerror)
+  (defun my-ack-and-a-half-wrapper (root search)
+    (ack-and-a-half search 'nil root))
+  (setq my-project-find-fallback-func 'my-ack-and-a-half-wrapper))
+
