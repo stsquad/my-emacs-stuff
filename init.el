1dc1280afd76f8074e39fd61ff387457d87f4711
Alex Bennée <alex@bennee.com>
2013-10-11 09:54:48 +0100

    init.el: add my-package-reset
    This will reset all the packages to a known manifest. I've made it a
    function rather than call it on start-up as it will kill installed
    packages not in the manifest.

--- init.el
+++ init.el
@@ -177,7 +177,7 @@
   (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives
-	       '("org" . "http://orgmode.org/elpa/") t)
+               '("org" . "http://orgmode.org/elpa/") t)
   
   ; filter some packages out
   (setq package-filter-function
@@ -185,16 +185,59 @@
         (or (not (string-equal archive "melpa"))
             (not (memq package '(magit org))))))
 
-  ; list of packages I care about
-  (defvar ajb-packages
-    '(ack-and-a-half expand-region magit magithub python
-		     solarized-theme zenburn-theme dynamic-fonts))
+  (unless (package-installed-p 'package+)
+     (package-install 'package+))
 
-  ; check what's installed
-  (defun ajb-packages-installed-p ()
-    (loop for p in ajb-packages
-	  when (not (package-installed-p p)) do (return nil)
-	  finally (return t))))
+  ;; This is dangerous to call in init.el as it will remove
+  ;; all packages not explicitly in the manifest. This may be helpful
+  ;; to keep packages clean but it breaks stuff you might be playing
+  ;; with between boots.
+  (defun my-packages-reset()
+    "Reset package manifest to the defined set"
+    (interactive)
+    (package-manifest 'ac-dabbrev 'ac-helm 'ac-js2 'ace-jump-mode
+                      'ack-and-a-half 'apache-mode
+                      'auto-complete
+                      'backtrace-mode
+                      'circe
+                      'dynamic-fonts
+                      'elpy
+                      'edebug-x
+                      'expand-region
+                      'git-blame
+                      'git-commit-mode
+                      'gitconfig-mode
+                      'gitignore-mode
+                      'gplusify
+                      'helm
+                      'helm-ack
+                      'helm-git-grep
+                      'helm-c-yasnippet
+                      'helm-themes
+                      'htmlize
+                      'ido-better-flex
+                      'js2-mode
+                      'json-mode
+                      'keychain-environment
+                      'litable
+                      'lusty-explorer
+                      'magit
+                      'magithub
+                      'multiple-cursors
+                      'org
+                      'org-trello
+                      'ox-reveal
+                      'package+
+                      'paredit
+                      'pastebin
+                      'protobuf-mode
+                      'rainbow-delimiters
+                      'smex
+                      'ssh-config-mode
+                      'solarized-theme
+                      'tracking
+                      'web-mode
+                      'zenburn-theme)))
 
 ;; Add local search path
 ;
