f2f5a7aa6d60dc81bf0faa14c479b291dde5e171
Alex Bennée <alex@bennee.com>
2013-11-06 16:01:45 +0000

    run_travis_tests.sh: initialise all packages and retest start-up

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -15,7 +15,22 @@ ${EMACS} --daemon
 OK=`emacsclient -e "(if I-completed-loading-dotinit 0 -1)"`
 if [ "$OK" != "0" ]
 then
-    echo "failed start-up"
+    echo "Failed --daemon start-up with clean package list"
+    exit -1
+fi
+
+# Now we have started we can install our normal package set
+INSTALL=`emacsclient -e "(my-packages-reset)"`
+echo "Install: $INSTALL"
+
+emacsclient -e "(save-buffers-kill-emacs 't)"
+sleep 10
+
+${EMACS} --daemon
+OK=`emacsclient -e "(if I-completed-loading-dotinit 0 -1)"`
+if [ "$OK" != "0" ]
+then
+    echo "Failed --daemon start-up with normal package list"
     exit -1
 fi
 
