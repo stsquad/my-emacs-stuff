ae0894ee2674982ff711b0d6b44c65a8b2be4834
Alex Bennée <alex@bennee.com>
2013-11-07 12:40:27 +0000

    tests/my-ert.el: add ert compile tests for all .el files
    This will compile test every package in my .emacs.d hierarchy

---
+++ tests/my-ert.el
@@ -0,0 +1,24 @@
+;;
+;; My ERT tests
+;;
+
+(require 'ert)
+
+;; thanks to spacebat on #emacs for helping with this one
+(defun my-define-compile-test (file)
+  "Define a single compile test for one of my .emacs modules"
+  (let* ((short-name
+          (car
+           (split-string (file-name-nondirectory file) "\\.")))
+         (test-name (intern (format "compile-%s" short-name))))
+    (eval `(ert-deftest ,test-name ()
+             (let ((byte-compile-error-on-warn t))
+               (should (byte-compile-file ,file)))))))
+
+;; Define a test for each elisp file in the directory
+(let*
+   ((root-dir (file-name-directory
+               (file-chase-links (locate-library "init.el"))))
+    (lisp-files (directory-files root-dir t "\.el$")))
+  (mapc 'my-define-compile-test lisp-files))
+  
