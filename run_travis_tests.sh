027aae85b5b8f16283c171d2bc8a12eccbcf91cc
Alex Bennée <alex@bennee.com>
2013-11-12 13:19:12 +0000

    run_tests.sh: cleaned up logic
    I've started splitting all the modules out so they can be compiled on
    their own. Also I do the compile tests without ert so each can be done
    in isolation.
    
    This also simplifies the --daemon stuff as I just have it run up twice
    now, once with bare bones and once with all packages.

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -21,26 +21,16 @@ then
     echo "Failed --daemon start-up with clean package list"
     exit -1
 fi
+emacsclient -e "(kill-emacs)"
 
 # Now we have started we can install our normal package set
 if [ "$EMACS" = 'emacs23' ] ; then
     # For Emacs23 we need to manually install ELPA
-    emacsclient -e "(kill-emacs)"
     ${EMACS} -q --batch -l ./tests/install-elpa.el -e "(my-install-elpa)"
-    ${EMACS} --daemon
 fi
 
-# Install the new package set
-INSTALL=`emacsclient -e "(my-packages-reset)"`
-echo "Install: $INSTALL"
-sleep 30
-# dump the current list
-INSTALLED=`emacsclient -e "package-activated-list"`
-sleep 30
-
-# seems a little drastic but it works
-emacsclient -e "(kill-emacs)"
-sleep 10
+# Install all the packages we use
+${EMACS} -q --batch -l ./my-package.el -e "(my-packages-reset)"
 
 ${EMACS} --daemon
 OK=`emacsclient -e "(if I-completed-loading-dotinit 0 -1)"`
@@ -48,11 +38,17 @@ if [ "$OK" != "0" ]
 then
     echo "Failed --daemon start-up with normal package list"
     exit -1
+else
+    INSTALLED=`emacsclient -e "package-activated-list"`
+    echo "Fully loaded --daemon with pkg=${INSTALLED}"
 fi
+emacsclient -e "(kill-emacs)"
+
+#
+# Now check we can compile everything
+${EMACS} -q --batch -l "tests/compile-setup.el" -f batch-byte-compile ~/.emacs.d/*.el
 
 # Now we are set-up we can the ERT tests (if we are running Emacs24 or above)
-if [ "$EMACS" = 'emacs23' ] ; then
-    ${EMACS} -q --batch -l "~/.emacs.d/init.el" -f batch-byte-compile ~/.emacs.d/*.el
-else
-    ${EMACS} -q --batch -l "~/.emacs.d/init.el" -l ert -l "tests/my-ert.el" -f ert-run-tests-batch-and-exit
+if [ "$EMACS" = 'emacs24' ] ; then
+    ${EMACS} -q --batch -l "tests/my-ert.el" -f ert-run-tests-batch-and-exit
 fi
---
+++ tests/compile-setup.el
@@ -0,0 +1,6 @@
+;;
+;; Set up for byte-compiling
+;;
+
+(add-to-list 'load-path "~/.emacs.d")
+(setq byte-compile-error-on-warn t)
