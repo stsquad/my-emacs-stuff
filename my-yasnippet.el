c826e7d222aae38258c01d0e4e36d90ba0af9c9b
Alex Bennée <alex@bennee.com>
2013-11-14 16:33:18 +0000

    my-yasnippet: prefer ido completion prompt to choose-value

--- my-yasnippet.el
+++ my-yasnippet.el
@@ -7,6 +7,9 @@
 (when (file-exists-p "~/.emacs.d/my-snippets")
   (add-to-list 'yas-snippet-dirs "~/.emacs.d/my-snippets"))
 
+(setq yas-prompt-functions
+      '(yas-ido-prompt yas-completing-prompt yas-no-prompt))
+
 ;; TODO add helper functions
 
 (yas-global-mode)
