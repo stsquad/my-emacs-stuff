2d41563c3bd6f0da5b171d64f3a944e145aedf60
Alex Bennée <alex@bennee.com>
2013-12-03 13:02:55 +0000

    my-c-mode: add more complete qemu style from pmm

--- my-c-mode.el
+++ my-c-mode.el
@@ -115,10 +115,34 @@
   '(
     "my-c-style"
     (indent-tabs-mode . nil)
-    (c-tab-always-indent . nil)
-    (c-comment-only-line-offset 0)
     (c-basic-offset . 4)
-    (c-offsets-alist . ((case-label . 0)))))
+    (tab-width . 8)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((substatement-open before after)))
+    (c-offsets-alist . ((case-label . 0)
+                        (statement-block-intro . +)
+                        (substatement-open . 0)
+                        (label . 0)
+                        (statement-cont . +)
+                        (innamespace . 0)
+                        (inline-open . 0)
+                        ))
+    (c-hanging-braces-alist .
+                            ((brace-list-open)
+                             (brace-list-intro)
+                             (brace-list-entry)
+                             (brace-list-close)
+                             (brace-entry-open)
+                             (block-close . c-snug-do-while)
+                             ;; structs have hanging braces on open
+                             (class-open . (after))
+                             ;; ditto if statements
+                             (substatement-open . (after))
+                             ;; and no auto newline at the end
+                             (class-close)
+                             ))
+    )
+  "QEMU C Programming Style")
 
 (c-add-style "qemu-c-style" qemu-c-style)
 
