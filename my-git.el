a260e8fb688d825fd94b90cd80bd0ab4ea285ea2
Alex Bennée <alex@bennee.com>
2013-12-13 17:50:18 +0000

    my-git.el: initial skeleton for checkpatch script

--- my-git.el
+++ my-git.el
@@ -19,11 +19,34 @@
 
 (global-set-key (kbd "C-x g") 'my-magit-start)
 
+;;;
+;; Run checkpatch.pl if we can
+;;
+(defvar magit-checkpatch-script nil
+  "Path to local checkpatch script, if it exists.")
+(make-variable-buffer-local 'magit-checkpatch-script)
+(put 'magit-checkpatch-script 'permanent-local t)
+
+(defun my-magit-run-checkpatch ()
+  "Run a checkpatch script against current commit."
+  (interactive)
+  (when (derived-mode-p 'magit-log-mode)
+    (message "do something here")))
+
+(defun my-magit-add-checkpatch-hook ()
+  "Add the ability to run a checkpatch script if we can."
+  (let ((script (concat default-directory "scripts/checkpatch.pl")))
+    (when (file-exists-p script)
+      (setq magit-checkpatch-script script)
+      (local-set-key (kbd "c") 'my-magit-run-checkpatch))))
+
 (eval-after-load "magit"
   '(progn
      (add-hook 'magit-mode-hook #'(lambda() (yas-minor-mode -1)))
      (add-hook 'magit-commit-mode-hook #'(lambda() (auto-fill-mode 1)))
-     (add-hook 'magit-log-edit-mode-hook #'(lambda() (auto-fill-mode 1)))
+     (add-hook 'magit-log-edit-mode-hook #'(lambda() (auto-fill-mode
+  1)))
+     (add-hook 'magit-log-mode-hook 'my-magit-add-checkpatch-hook)
      (setq
       magit-status-buffer-switch-function 'switch-to-buffer
       magit-rewrite-inclusive 'nil)))
