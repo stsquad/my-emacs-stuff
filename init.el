5c9dbd6c1a99155369ba677dad817f30f02f768a
Alex Bennée <alex.bennee@linaro.org>
2014-05-13 17:11:32 +0100

    my-windows.el: new file for window navigation
    I've also re-bound C-x o to my-other-ace-window when I have ace-window
    available to play with.

--- init.el
+++ init.el
@@ -243,56 +243,18 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-;; Window navigation and size control
-(when (maybe-load-library "windmove")
-  (windmove-default-keybindings))
-
-(unless (and (fboundp 'crmbk-running-in-host-x11-p)
-             (crmbk-running-in-host-x11-p))
-  (global-set-key (kbd "<M-down>") 'enlarge-window)
-  (global-set-key (kbd "<M-right>") 'enlarge-window-horizontally)
-  (global-set-key (kbd "<M-up>") 'shrink-window)
-  (global-set-key (kbd "<M-left>") 'shrink-window-horizontally))
 
 ;(global-set-key (kbd "<C-tab>") 'bury-buffer)
 (global-set-key (kbd "<C-tab>") 'pop-global-mark)
 
-;; Allow windows to be dedicated to one thing interactively
-;; Toggle window dedication
-(defun toggle-window-dedicated ()
-  "Toggle whether the current active window is dedicated or not."
-  (interactive)
-  (message
-   (if (let (window (get-buffer-window (current-buffer)))
-         (set-window-dedicated-p window
-                                 (not (window-dedicated-p window))))
-       "Window '%s' is dedicated"
-     "Window '%s' is normal")
-   (current-buffer)))
-
-;; Press [pause] key in each window you want to "freeze"
-(global-set-key [pause] 'toggle-window-dedicated)
-
-(defun toggle-frame-split ()
-  "If the frame is split vertically, split it horizontally or vice versa.
-Assumes that the frame is only split into two."
-  (interactive)
-  (unless (= (length (window-list)) 2)
-    (error "Can only toggle a frame split in two"))
-  (let ((split-vertically-p (window-combined-p)))
-    (delete-window) ; closes current window
-    (if split-vertically-p
-        (split-window-horizontally)
-      (split-window-vertically)) ; gives us a split with the other window twice
-    (switch-to-buffer nil))) ; restore the original window in this part of the frame
-;; I don't use the default binding of 'C-x 5', so use toggle-frame-split instead
-(global-set-key (kbd "C-x %") 'toggle-frame-split)
 
 ;; Handle next/prev error on keymap / and * (with numlock off)
 (global-set-key (kbd "M-O o") 'previous-error)
 (global-set-key [kp-divide] 'previous-error)
 (global-set-key (kbd "M-O j") 'next-error)
 (global-set-key [kp-multiply] 'next-error)
+
+(require 'my-windows)
 
 (when (require 'helm nil t)
   (load-library "my-helm.el"))
