d8b390d6d88107f28d1bf81b3799cbe0f341c2f9
Alex Bennée <alex.bennee@linaro.org>
2014-06-30 09:29:35 +0100

    my-helm: add helm-gtags

--- my-helm.el
+++ my-helm.el
@@ -29,6 +29,23 @@
 (when (fboundp 'helm-descbinds)
   (global-set-key (kbd "C-h h") 'helm-descbinds))
 
+(when (locate-library "helm-gtags")
+    ;; Enable helm-gtags-mode
+    (add-hook 'c-mode-hook 'helm-gtags-mode)
+    (add-hook 'c++-mode-hook 'helm-gtags-mode)
+    (add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+    ;; Set key bindings
+    (eval-after-load "helm-gtags"
+      '(progn
+         (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+         (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+         (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+;         (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
+         (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+         (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+         (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))))
+
 ;; Occur stuff
 (cond
  ((fboundp 'helm-swoop)
