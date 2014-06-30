f74d4fdc8578997cfe616e6be8215a068c3c0b9e
Alex Bennée <alex.bennee@linaro.org>
2014-06-30 08:45:46 +0100

    my-package.el: remove emms from defaults, add ace-jump-buffer

--- my-package.el
+++ my-package.el
@@ -10,7 +10,7 @@
 
 (defvar my-useful-packages
   '(ac-etags ac-c-headers ac-dabbrev ac-helm ac-js2
-    ace-jump-mode
+    ace-jump-mode ace-jump-buffer
     ack-and-a-half
     android-mode
     apache-mode
@@ -19,7 +19,6 @@
     circe
     edit-server edit-server-htmlize
     elpy
-    emms
     eproject
     expand-region
     flycheck
