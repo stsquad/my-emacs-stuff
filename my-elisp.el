9eea13f497e071cf6da99c27dfbe8f673a72c393
Alex Bennée <alex.bennee@linaro.org>
2015-06-10 14:47:08 +0100

    my-elisp: only enable smartparens if we have it

--- my-elisp.el
+++ my-elisp.el
@@ -33,7 +33,8 @@
   (setq mode-name "elisp")
   (eldoc-mode t)
   (local-set-key (kbd "C-c C-c") 'my-elisp-compile-buffer)
-  (show-smartparens-global-mode)
+  (when (boundp show-smartparens-global-mode)
+    (show-smartparens-global-mode))
   (turn-on-auto-fill))
 
 (use-package lisp-mode
