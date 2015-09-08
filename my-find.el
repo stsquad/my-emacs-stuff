9e6ac1e24511225c469636c1eb3d8c6a215f4321
Alex Bennée <alex.bennee@linaro.org>
2015-09-08 12:18:15 +0100

    my-find/my-eproject: burn the old code with fire
    No need to re-invent and patch when I can have the tools where I want
    them.

--- my-find.el
+++ my-find.el
@@ -1,62 +1,32 @@
+;;; my-find --- General searching configuration
 ;;
-;; Find methods
+;; Copyright (C) 2014 Alex Bennée
 ;;
-;
-; I want two find methods:
-;   f5 - from the "project" root
-;   f6 - from where I am (or somewhere else)
-;
+;; Author: Alex Bennée <alex@bennee.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;;; Commentary:
+;;
+;; This provides a very basic find front-end for f5/f6. It mostly
+;; defers to packages and leaves f5 to be over-ridden when running
+;; under a project mode.
+;;
+;;; Code:
+
+;; Require prerequisites
+
+;; Variables
+
+;; Code
 
 (require 'use-package)
 
-(defvar my-find-history
-  '()
-  "History of searches I've done")
-
-(defun my-find-search-string()
-  "Return a historical based search string via ido"
-  (ido-completing-read "Search string:"
-		       my-find-history ; sources
-		       'nil ; predicate
-		       'nil ; require-match
-		       (thing-at-point 'symbol) ; initial input
-		       'my-find-history)) ;history
-
-(defun my-find-paramters()
-  "Return a root directory and search string combo"
-  (list
-   (read-directory-name "Root dir:")
-   (my-find-search-string)))
-  
-(defun my-grep-find (&optional root search)
-  (interactive (my-find-paramters))
-  (grep-find (concat
-	      "find "
-	      root " -print0 |  xargs -0 -e grep -n -e " search)))
-
-(defvar my-project-find-fallback-func
-  'my-grep-find
-  "The function I call to do the actual search")
-
-;;   (interactive (list
-;;                 (eproject--do-completing-read
-;;                  "Project name: " (eproject-project-names))))
-
-(defun my-project-find (prefix &optional search)
-  "Search within the project. If prefix is set prompt for a directory."
-  (interactive (list (my-find-search-string)))
-  (funcall my-project-find-fallback-func default-directory search))
-
-(global-set-key (kbd "<f5>") 'my-project-find)
-
-;;
-;; <f6> for project search, last successful one wins
-;;
-
-(global-set-key (kbd "<f6>") '(lambda (root search)
-                                (interactive (my-find-paramters))
-                                (funcall my-project-find-fallback-func
-                                         root search)))
 (use-package ack-and-a-half
   :commands ack-and-a-half
   :init (global-set-key (kbd "<f6>") 'ack-and-a-half))
@@ -68,4 +38,17 @@
 (use-package swiper
   :bind ("C-s" . swiper))
 
+(defun my-project-find (prefix)
+  "Search within the default-directory. This is mainly for
+  the benefit of things like mail. Pass the prefix to the ultimate
+  function."
+  (interactive "P")
+  (if (and (file-directory-p (format "%s.git/" default-directory))
+           (functionp 'helm-git-grep))
+      (call-interactively #'helm-git-grep)
+    (call-interactively #'helm-do-ag default-directory)))
+
+(global-set-key (kbd "<f5>") 'my-project-find)
+
 (provide 'my-find)
+;;; my-find.el ends here
