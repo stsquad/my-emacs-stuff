c39ab1e1ddf0310de27571a0fd9d691bbeb89379
Alex Bennée <alex.bennee@linaro.org>
2016-02-02 11:33:24 +0000

    my-compat: add a compatibility hack

---
+++ my-compat.el
@@ -0,0 +1,19 @@
+;;; my-compat --- compatibility hacks
+;;
+;;; Commentary:
+;;
+;; While I use emacs24 across the board there is some stuff that was
+;; introduced a little later.
+;;
+;;; Code:
+
+(when (not (symbolp 'with-eval-after-load))
+    (defmacro with-eval-after-load (file &rest body)
+      "Execute BODY after FILE is loaded.
+FILE is normally a feature name, but it can also be a file name,
+in case that file does not provide any feature."
+      (declare (indent 1) (debug t))
+      `(eval-after-load ,file (lambda () ,@body))))
+
+(provide 'my-compat)
+;;; my-compat.el ends here
