839a55bdb1bbe8b3c979ececf3cebfc181418ba3
Alex Bennée <alex.bennee@linaro.org>
2016-03-08 16:39:44 +0000

    init.el: comment cleanup

--- init.el
+++ init.el
@@ -22,7 +22,6 @@
   (setq load-prefer-newer t))
 
 ;; Manually load this (as paths not yet set)
-;(eval-when-compile (defvar god-local-mode))
 (declare-function my-add-config-paths "~/.emacs.d/my-elisp/my-paths" t t)
 (when (load-library "~/.emacs.d/my-elisp/my-paths")
   (my-add-config-paths))
