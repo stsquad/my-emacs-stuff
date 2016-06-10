80253ac66f879e9ef36ba06c4c8e63bb98a11a6d
Alex Bennée <alex.bennee@linaro.org>
2016-06-10 20:05:21 +0100

    my-dired: a little default-directory hydra hack

--- my-dired.el
+++ my-dired.el
@@ -7,6 +7,8 @@
 ;;; Code:
 
 (require 'use-package)
+(require 'my-utils)
+(require 'my-hydra)
 
 (defun my-dired-enable-recursive-delete ()
   "Enable easy recursive delete for temporary directories."
@@ -19,6 +21,20 @@
   :config (add-hook 'dired-mode-hook 'my-dired-enable-recursive-delete))
 
 (use-package dired-async)
+
+(use-package dired-quick-sort
+  :config (dired-quick-sort-setup))
+
+(global-set-key
+   (kbd "C-x d")
+   (defhydra my-hydra-directory (:exit t :hint nil :color red :timeout 5)
+     (concat "in %`default-directory "
+      "dired _b_rowse, _s_et new default-directory, set to _l_ast dir %`my-last-set-directory")
+     ;;
+     ("b" ido-dired)
+     ("s" my-set-default-directory)
+     ("l" (lambda () (interactive) (my-set-default-directory my-last-set-directory)))))
+
 
 (provide 'my-dired)
 ;;; my-dired.el ends here
