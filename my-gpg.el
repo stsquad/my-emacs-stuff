82c72dc8045d25d6d87738eee72d45c2f461a1e4
Alex Bennée <alex.bennee@linaro.org>
2016-07-21 16:43:33 +0100

    my-gpg: move gpg utils into the my-gpg.el

--- my-gpg.el
+++ my-gpg.el
@@ -26,6 +26,19 @@
   :defer 60
   :config (keychain-refresh-environment))
 
+;; Fix up the frame so we don't send pinentry to the wrong place
+(defun my-fixup-gpg-agent (frame)
+  "Tweak DISPLAY and GPG_TTY environment variables as appropriate to `FRAME'."
+  (when (fboundp 'keychain-refresh-environment)
+    (keychain-refresh-environment))
+  (if (display-graphic-p frame)
+      (setenv "DISPLAY" (terminal-name frame))
+    (setenv "GPG_TTY" (terminal-name frame))
+    (setenv "DISPLAY" nil)))
+
+(add-hook 'after-make-frame-functions 'my-fixup-gpg-agent)
+(add-hook 'focus-in-hook #'(lambda() (my-fixup-gpg-agent (selected-frame))))
+
 ;; enable EasyPG handling
 ; gpg-agent confuses epa when getting passphrase
 (defun my-squash-gpg (&rest ignored-frame)
