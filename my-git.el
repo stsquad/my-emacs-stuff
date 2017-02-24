bbef6553a363a256143e773468afd2d984e047d7
Alex Bennée <alex.bennee@linaro.org>
2017-02-24 11:04:44 +0000

    my-git: drop magit checkpatch hacks
    These are all in checkpatch-mode now.

--- my-git.el
+++ my-git.el
@@ -40,12 +40,11 @@
                       'my-magit-section-visibilities)
           (remove-hook 'magit-section-set-visibility-hook 'my-magit-section-visibilities)
         (add-hook 'magit-section-set-visibility-hook
-                  'my-magit-section-visibilities t t)))
+                  'my-magit-section-visibilities t)))
 
     (add-hook 'magit-section-set-visibility-hook 'my-magit-section-visibilities t)
     (add-hook 'magit-mode-hook #'(lambda() (yas-minor-mode -1)))
     (add-hook 'magit-log-edit-mode-hook #'(lambda() (auto-fill-mode 1)))
-    (add-hook 'magit-log-mode-hook 'my-magit-add-checkpatch-hook)
     (setq
      ;; really I never use anything but git
      vc-handled-backends nil
@@ -134,55 +133,6 @@ Navigate History   : _p_revious/_b_ack history: %(nth 3 (car help-xref-stack)) _
       (let ((msg (match-string-no-properties 1)))
         (when (my-org-find-review-comments msg)
           (git-rebase-edit))))))
-
-;;;
-;; Run checkpatch.pl if we can
-;;
-(defvar magit-checkpatch-script nil
-  "Path to local checkpatch script, if it exists.")
-(make-variable-buffer-local 'magit-checkpatch-script)
-(put 'magit-checkpatch-script 'permanent-local t)
-
-(use-package compile
-  :commands compilation-mode
-  :config
-  (progn
-    ;; Match checkpatch.pl output
-    (add-to-list
-     'compilation-error-regexp-alist-alist
-     '(checkpatch
-       "\\(WARNING\\|ERROR\\).*\n#.*FILE: \\([^:]+\\):\\([^:digit:]+\\).*\n.*"
-       2 ; file
-       3 ; line
-       ))
-    ;; add reference
-    (add-to-list 'compilation-error-regexp-alist 'checkpatch)))
-
-(defun my-magit--do-run-checkpatch (commit)
-  "Run the checkpatch script against `COMMIT'."
-  (let ((proc-name "checkpatch")
-        (buff-name (format "checkpatch-%s" commit)))
-    (start-process-shell-command
-     proc-name
-     buff-name
-     (format "git show --pretty=email %s | %s -" commit magit-checkpatch-script))
-    (switch-to-buffer buff-name)
-    (goto-char (point-min))
-    (compilation-minor-mode)))
-
-(defun my-magit-run-checkpatch ()
-  "Run a checkpatch script against current commit."
-  (interactive)
-  (when (derived-mode-p 'magit-log-mode)
-    (my-magit--do-run-checkpatch (magit-commit-at-point))))
-
-(defun my-magit-add-checkpatch-hook ()
-  "Add the ability to run a checkpatch script if we can."
-  (let ((script (concat default-directory "scripts/checkpatch.pl")))
-    (when (file-exists-p script)
-      (setq magit-checkpatch-script script)
-      (local-set-key (kbd "C") 'my-magit-run-checkpatch))))
-
 
 ;;;
 ;;; Additional GIT bits
