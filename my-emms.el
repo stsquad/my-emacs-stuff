6ea42e0f62f7174811ca5bade64e3b010b6bab1c
Alex Bennée <alex.bennee@linaro.org>
2017-03-01 19:56:23 +0000

    my-emms: simplified a chunk

--- my-emms.el
+++ my-emms.el
@@ -1,42 +1,40 @@
+;;; my-emms --- EMMS Setup
 ;;
-;; EMMS Setup
+;;; Commentary:
 ;;
+;; 
+;;
+;;; Code:
 
-(require 'emms-setup)
-(require 'emms-info)
-(require 'emms-info-mp3info)
-(require 'emms-info-ogginfo)
-(require 'emms-browser)
+;; Require prerequisites
+(eval-when-compile (require 'use-package))
+(require 'my-vars)
 
-(emms-all)
-(emms-default-players)
+(use-package emms-setup
+  :config
+  (emms-all)
+  (emms-default-players))
 
-;;(remove-hook 'emms-player-started-hook 'emms-show)
-(setq emms-show-format "NP: %s")
-;(setq emms-track-description-function emms-track-simple-description)
-
-;; When asked for emms-play-directory,
-;; always start from this one 
-(setq emms-source-file-default-directory (if I-am-at-work
-                                             "/export/music"
-                                           "/home/alex/Music")
-      emms-playlist-buffer-name "*Music*"
-      emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
-
-;; (setq emms-track-description-function 'fc-emms-track-description)
-;; (defun fc-emms-track-description (track)
-;;   "Return a nice description of TRACK."
-;;   (let ((desc (emms-track-simple-description track)))
-;;     (if (string-match "^/home/forcer/snd/\\(musik/\\(artists/\\)?\\)?\\(.*\\)" 
-;; desc)
-;;         (match-string 3 desc)
-;;       desc)))
-
-(global-set-key (kbd "<XF86AudioPlay>") 'emms-pause)
-(define-key dired-mode-map (kbd "C-q") 'emms-add-dired)
+(use-package emms
+  :map
+  :config
+  (setq emms-source-file-default-directory (if I-am-at-work
+                                               "/export/music"
+                                             "/home/alex/media/music")
+        emms-playlist-buffer-name "*Music*"
+        emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find
+        emms-show-format "NP: %s"
+        emms-track-description-function 'emms-track-simple-description))
 
 
-(require 'emms-mode-line)
-(emms-mode-line nil)
+(use-package emms-info-mp3info)
+(use-package emms-info-ogginfo)
+(use-package emms-browser)
 
+(provide 'my-emms)
+;;; my-emms.el ends here
+;;       emms-playlist-buffer-name "*Music*"
+;;       emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
 
+;; (global-set-key (kbd "<XF86AudioPlay>") 'emms-pause)
+;; (define-key dired-mode-map (kbd "C-q") 'emms-add-dired)
