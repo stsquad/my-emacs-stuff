92455f0573f7002517e6878791f43845696a33a1
Alex Bennée <alex.bennee@linaro.org>
2017-08-09 15:48:16 +0100

    atomic-chrome: clean-up adding to list
    We defer loading atomic-chrome, so we should defer setting it's config
    variables for the other modes we use.

--- my-atomic-chrome.el
+++ my-atomic-chrome.el
@@ -27,23 +27,24 @@
 ;; Handy for wiki editing
 (use-package mediawiki
   :ensure t
-  :commands mediawiki-mode
-  :init
-  (progn
-    (add-to-list 'atomic-chrome-url-major-mode-alist
-                 '("mediawiki" . mediawiki-mode))
-    (add-to-list 'atomic-chrome-url-major-mode-alist
-                 '("wikipedia" . mediawiki-mode))
-    (add-to-list 'atomic-chrome-url-major-mode-alist
-                 '("wiki.qemu.org" . mediawiki-mode))))
+  :commands mediawiki-mode)
 
 (use-package moinmoin-mode
-  :commands moinmoin-mode
-  :init (add-to-list 'atomic-chrome-url-major-mode-alist
-                 '("wiki.linaro.org" . moinmoin-mode)))
+  :commands moinmoin-mode)
 
-(add-to-list 'atomic-chrome-url-major-mode-alist
-             '("www.bennee.com/~alex/blog" . web-mode))
+;; Defer setting up handlers until after atomic-chrome has loaded
+(with-eval-after-load 'atomic-chrome
+  (add-to-list 'atomic-chrome-url-major-mode-alist
+               '("mediawiki" . mediawiki-mode))
+  (add-to-list 'atomic-chrome-url-major-mode-alist
+               '("wikipedia" . mediawiki-mode))
+  (add-to-list 'atomic-chrome-url-major-mode-alist
+               '("wiki.qemu.org" . mediawiki-mode))
+  (when (package-installed-p 'moinmoin-mode)
+    (add-to-list 'atomic-chrome-url-major-mode-alist
+                 '("wiki.linaro.org" . moinmoin-mode)))
+  (add-to-list 'atomic-chrome-url-major-mode-alist
+               '("www.bennee.com/~alex/blog" . web-mode)))
 
 (provide 'my-atomic-chrome)
 ;;; my-atomic-chrome.el ends here
