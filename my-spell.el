3b5047a1695248e80fef6075997c340911909c9c
Alex Bennée <alex.bennee@linaro.org>
2019-11-21 17:33:04 +0000

    my-spell: cleanup spelling setup

--- my-spell.el
+++ my-spell.el
@@ -8,41 +8,37 @@
 
 (require 'my-utils)
 
+(use-package ispell
+  :config
+  (let ((spell-path (which-lookup '("aspell" "ispell")))) ; aspell is preferred
+    (when spell-path
+      (setq ispell-program-name spell-path
+            ispell-dictionary "british"))))
+
 (use-package flyspell
   :commands (flyspell-mode flyspell-prog-mode)
+  :after ispell
   :diminish "fs"
-  :init
-  (progn
-    ;; Add hooks to enable flyspell
-    (add-hook 'text-mode-hook 'turn-on-flyspell)
-    (add-hook 'c-mode-common-hook 'flyspell-prog-mode)
-    (add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
-    ;; Hooks for non-possessive its
-    (add-hook 'text-mode-hook 'my-hightlight-non-possessive-its)
-    (add-hook 'prog-mode-hook 'my-hightlight-non-possessive-its))
-  :config
-  (progn
-    (use-package ispell
-      :config
-      (let ((spell-path (which-lookup '("aspell" "ispell")))) ; aspell is preferred
-        (when spell-path
-          (setq ispell-program-name spell-path
-                ispell-dictionary "british"))))))
-  
+  :hook ((text-mode-hook . turn-on-flyspell)
+         (prog-mode-hook . flyspell-prog-mode)))
+
 (defun turn-on-flyspell ()
   "Force 'flyspell-mode' on using a positive arg.  For use in hooks."
   (interactive)
   (flyspell-mode 1))
 
-(use-package hi-lock
-  :commands hi-lock-face-phrase-buffer
-  :config (eval-after-load "hi-lock"
-            '(assq-delete-all 'hi-lock-mode minor-mode-map-alist)))
-
-(defun my-hightlight-non-possessive-its ()
+(defun my-highlight-non-possessive-its ()
   "Turn on hi-lock mode for any (potentially incorrect) usage of it's"
   (interactive)
   (hi-lock-face-phrase-buffer "it's"))
+
+(use-package hi-lock
+  :commands hi-lock-face-phrase-buffer
+  :hook ((text-mode-hook . my-highlight-non-possessive-its)
+         (prog-mode-hook . my-highlight-non-possessive-its))
+  :config (eval-after-load "hi-lock"
+            '(assq-delete-all 'hi-lock-mode minor-mode-map-alist)))
+
 
 (provide 'my-spell)
 ;;; my-spell.el ends here
