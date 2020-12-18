c2de6cca3f2f5dee906b3bfc36bc10365bb96255
Alex Bennée <alex.bennee@linaro.org>
2020-12-18 17:57:27 +0000

    my-email: move mu binary to it's own install dir

--- my-email.el
+++ my-email.el
@@ -76,7 +76,7 @@
                    "~/src/emacs/mu/install/share/emacs/site-lisp/mu4e/")))
   (setq mu4e-mu-binary (or
                         (my-return-path-if-ok "/usr/bin/mu")
-                        (my-return-path-if-ok "~/bin/mu")))
+                        (my-return-path-if-ok "~/src/emacs/mu/install/bin/mu")))
   (when local-mu4e
     (add-to-list 'load-path local-mu4e)))
 
