c1d5b86f21b2b0d5f27981c0a4f8dde93933e440
Alex Bennée <alex.bennee@linaro.org>
2021-03-31 15:35:35 +0000

    my-display: fix a style for non-X displays
    Most of my remote terminals have truecolour now so lets pick something
    that looks nice enough. Also help us find list-colors-display.

--- my-display.el
+++ my-display.el
@@ -196,10 +196,19 @@
         "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
         "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
 
+(use-package facemenu
+  :commands list-colors-display)
+
 (use-package moe-theme
   :ensure t
   :commands moe-dark
-  :init (moe-dark))
+  :init (when window-system
+          (moe-dark)))
+
+(use-package gruvbox-theme
+  :ensure t
+  :init (when (not window-system)
+          (load-theme 'gruvbox t)))
 
 (use-package zenburn-theme
   :ensure t)
