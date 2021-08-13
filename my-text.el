10e54c3099f3dd5ce1bc8794ef835706af95d118
Alex Bennée <alex.bennee@linaro.org>
2021-08-13 19:02:12 +0100

    my-text: avoid ugly subscripts in virtio text stuff

--- my-text.el
+++ my-text.el
@@ -9,5 +9,10 @@
 (use-package rst
   :mode ((rx (: ".rst" (zero-or-one ".inc") eol)) . rst-mode))
 
+(use-package tex-mode
+  :config (setq tex-verbatim-environments
+                '("verbatim" "verbatim*" "Verbatim" "lstlisting")
+                tex-fontify-script nil))
+
 (provide 'my-text)
 ;;; my-text.el ends here
