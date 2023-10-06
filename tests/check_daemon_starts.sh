5373d39f1b7fc7b2f0121e04717122873c48b442
Alex Bennée <alex.bennee@linaro.org>
2023-10-06 16:00:49 +0100

    tests: clean-up and convert to POSIX shell

--- tests/check_daemon_starts.sh
+++ tests/check_daemon_starts.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Check the Daemon starts up
 #
 set -ex
 emacs --version
-emacs --daemon
-OK=`emacsclient -e "(if I-completed-loading-dotinit 0 -1)"`
-if [ "$OK" != "0" ]
+emacs --daemon --debug-init
+
+sleep 1
+
+if test $(emacsclient -e "(if I-completed-loading-dotinit 0 -1)") -eq 0
 then
     echo "Failed --daemon start-up"
-    exit -1
+    exit 1
 else
-    INSTALLED=`emacsclient -e "package-activated-list"`
+    INSTALLED=$(emacsclient -e "package-activated-list")
     echo "Succesful --daemon start-up with pkg=${INSTALLED}"
 fi
 emacsclient -e "(kill-emacs)"
