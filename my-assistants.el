34750c9173101e30743f3600b6e8dcc6bbdc9882
Alex Bennée <alex.bennee@linaro.org>
2024-11-03 19:18:04 +0000

    my-assistants: setup gemini and codeium assistants

--- my-assistants.el
+++ my-assistants.el
@@ -22,13 +22,25 @@
 (use-package llm-openai
   :config (setq my-openai-llm
                 (make-llm-openai :key (my-pass-password "api.openai.com")
-                                 :chat-model "gpt-4")))
+                                 :chat-model "gpt-4o")))
 
 (use-package llm-ollama
   :config (setq my-ollama-codellama
                 (make-llm-ollama :chat-model "codellama")
                 my-ollama-mistral
                 (make-llm-ollama :chat-model "mistrel")))
+
+(use-package llm-gemini
+  :config (setq my-gemini-llm
+                (make-llm-gemini :key (my-pass-password "api.gemini.google.com")
+                                 :chat-model
+                                 "gemini-1.5-flash")
+                my-gemini-pro-llm
+                (make-llm-gemini :key (my-pass-password "api.gemini.google.com")
+                                 :chat-model "gemini-1.5-pro-latest")
+                my-gemma2-llm
+                (make-llm-gemini :key (my-pass-password "api.gemini.google.com")
+                                 :chat-model "gemma-2-27b-it")))
 
 ;;
 ;; The OG ChatGpt integration
@@ -79,10 +91,53 @@
 ;;
 (use-package ellama
   :ensure t
-  :init
-  (setopt ellama-language "English")
-  (setopt ellama-provider my-ollama-mistral))
+  :init (setopt ellama-language "English"
+                ellama-provider my-gemini-llm
+                ellama-providers '(("Gemini Pro" . my-gemini-pro-llm)
+                                   ("Gemini" . my-gemini-llm)
+                                   ("Gemma2" . my-gemma2-llm)
+                                   ("ChatGPT" . my-openai-llm))
+                ellama-keymap-prefix "C-c C-l l"))
 
+;;
+;; Codeium
+;;;
+
+;; https://github.com/Exafunction/codeium.el/issues/97#issuecomment-2354092579
+(defun my-codeium-wrapper ()
+  "Decouple codeium from other completions"
+  (interactive)
+  (cape-interactive #'codeium-completion-at-point))
+
+(use-package codeium
+  :load-path (lambda () (my-return-path-if-ok
+                         "~/src/emacs/codeium.el.git/"))
+  :bind (:map prog-mode-map
+              ("C-x c" . my-codeium-wrapper))
+  :config
+  (setq codeium/metadata/api_key (my-pass-password "api.codeium.com")
+        codeium-mode-line-enable (lambda (api)
+                                   (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
+  (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
+  ;; alternatively for a more extensive mode-line
+  ;; (add-to-list 'mode-line-format '(-50 "" codeium-mode-line) t)
+
+  ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
+  (setq codeium-api-enabled
+        (lambda (api)
+          (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
+  ;; You can overwrite all the codeium configs!
+  ;; for example, we recommend limiting the string sent to codeium for better performance
+  (defun my-codeium/document/text ()
+    (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
+  ;; if you change the text, you should also change the cursor_offset
+  ;; warning: this is measured by UTF-8 encoded bytes
+  (defun my-codeium/document/cursor_offset ()
+    (codeium-utf8-byte-length
+     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
+
+  (setq codeium/document/text 'my-codeium/document/text
+        codeium/document/cursor_offset 'my-codeium/document/cursor_offset))
 
 (provide 'my-assistants)
 ;;; my-assistants.el ends here
