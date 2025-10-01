a7344c0b041028402f95b4ee2ad93e0e8d925087
Alex Bennée <alex.bennee@linaro.org>
2025-10-01 17:00:52 +0100

    my-email: add a helper to archive an mu4e-headers
    Basically output a flat file with the textual contents of all the
    displayed emails.

--- my-email.el
+++ my-email.el
@@ -387,6 +387,36 @@ Useful for replies and drafts")
            (mu4e-thread-unfold)
          (mu4e-thread-fold)))
 
+(defun my-mu4e-archive-headers ()
+  "Archive all the emails in the headers currently displayed."
+    (interactive)
+    (let ((buffer
+           (get-buffer-create
+            (format "*Mu4e Archive of %s*" mu4e--search-last-query))))
+      ;; Add a pre-amble
+      (with-current-buffer buffer
+        (erase-buffer)
+        (goto-char 0)
+        (insert (format "Archive of email thread, generated at:")
+                (format-time-string "%d/%m/%y %H:%M")
+                "\n\n"))
+      ;; collect the message text into the buffer
+      (mu4e-headers-for-each
+       (lambda (msg)
+         (with-current-buffer buffer
+           (insert
+            (format "Message-Id: %s\n" (mu4e-message-field msg :message-id))
+            (mu4e-view-message-text msg)
+            (format "*End of Message*\n\n\n")))))
+      ;; prompt to write the file out
+      (let ((filename (read-file-name "Enter filename for archive:"
+                                      nil "archive.txt" nil)))
+        (when filename
+          (with-current-buffer buffer
+            (write-file (expand-file-name filename))
+          (message "Archived to %s" filename))))))
+
+
 
 (use-package mu4e-headers
   :commands mu4e-headers-mode
