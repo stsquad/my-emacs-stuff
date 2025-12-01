36153abeb669e63a4237ccb90797b8ec90b42869
Alex Bennée <alex.bennee@linaro.org>
2025-12-01 13:34:51 +0000

    my-elfeed: clean-up and ensure rss feeds are not cached

--- my-elfeed.el
+++ my-elfeed.el
@@ -7,10 +7,14 @@
 ;;
 ;;; Code:
 
+(eval-when-compile (require 'use-package))
+(use-package my-package)
+
 (use-package elfeed
   :ensure t
   :config (setq elfeed-log-level 'debug
-                elfeed-use-curl 't))
+                elfeed-use-curl 't
+                elfeed-curl-extra-arguments '("-H" "Cache-Control: no-cache")))
 
 (when have-melpa
   (use-package elfeed-org
